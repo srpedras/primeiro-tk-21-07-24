858c414e0e00c409dbdf97f99e1c3abdb33db652
srpedras.email <chicochiquimho@gmail.com>
2024-07-26 16:45:44 -0300

    etapa 3

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from tkinter import *
+from tkinter import messagebox
 
 
 
@@ -9,11 +10,11 @@ class Aplication():
     def __init__(self):
         self.janela = janela
         self.tela()
-        self.tema()
+        #self.tema()
         self.tela_login()
         janela.mainloop()
 
-    def tema(self):    
+    #def tema(self):    
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("dark-blue")
 
@@ -30,7 +31,7 @@ class Aplication():
         label_img = ctk.CTkLabel(master=janela, image=img, text=None)
         label_img.place(x=45, y=70)
         label_text = ctk.CTkLabel(master=janela, text='Bem vindo ao seu sitema cadastro', font = ('Roboto', 18), text_color= ('black'))
-        label_text.place(x=15,y=10)
+        label_text.place(x=20,y=15)
 
         #frame principal
         login_frame = ctk.CTkFrame(master=janela, width=370, height=396)
@@ -56,13 +57,67 @@ class Aplication():
         checkbox.place(x=25, y=235)
 
         #botão de login
-        login_button = ctk.CTkButton(master=login_frame, text="Login", width=300)
+        def login():
+            msg = messagebox.showinfo(title="Estado de Login",message="Parabens!login feito com sucesso.")
+            pass
+        login_button = ctk.CTkButton(master=login_frame, text="Login", width=300, command=login)
         login_button.place(x=25, y=285)
 
         #botão  cadastrar
         register_span = ctk.CTkLabel(master=login_frame, text="se não possui uma conta")
         register_span.place(x=20, y=330)
-        cadastro_button = ctk.CTkButton(master=login_frame, text="Cadastrar usuario", width=150, fg_color="green", hover_color="#2D9334")
+
+        def tela_register():
+            #remover o frame de login
+            login_frame.pack_forget()
+
+            #criando a tela de cadastro de usuario
+            rg_frame = ctk.CTkFrame(master=janela, width=370, height=396)
+            rg_frame.pack(side=RIGHT)
+
+            #frames de login
+            label = ctk.CTkLabel(master=rg_frame, text='Faça seu cadastro', font = ('Roboto', 20, 'bold'), text_color= ('black'))
+            label.place(x=25, y=35)
+
+            span = ctk.CTkLabel(master=rg_frame, text='*Por favor preencha todos os campos corretamente', font = ('Roboto', 12), text_color= "gray")
+            span.place(x=25, y=75)
+
+            username_login = ctk.CTkEntry(master=rg_frame, placeholder_text='Nome de usuario',width=300, font = ('Roboto',15))
+            username_login.place(x=25, y=105)
+
+            email_login = ctk.CTkEntry(master=rg_frame, placeholder_text='E-mail de usuario',width=300, font = ('Roboto',15))
+            email_login.place(x=25, y=145)
+
+            password_login = ctk.CTkEntry(master=rg_frame, placeholder_text='Login do usuario',width=300, font = ('Roboto',15), show="*")
+            password_login.place(x=25, y=185)
+
+            cpassword_login = ctk.CTkEntry(master=rg_frame, placeholder_text='Confirme o login',width=300, font = ('Roboto',15), show="*")
+            cpassword_login.place(x=25, y=225)
+
+            checkbox = ctk.CTkCheckBox(master=rg_frame, text="Aceito os termos e Politicas  da empresa")
+            checkbox.place(x=25, y=270)
+
+            #devolvendo o frame de login
+            def back():
+               rg_frame.pack_forget()
+
+               login_frame.pack(side=RIGHT)
+
+              
+
+            back_button = ctk.CTkButton(master=rg_frame, text="Voltar", width=145, fg_color="green", hover_color="#2D9334", command=back)
+            back_button.place(x=25, y=310)
+
+            #salvando os dados
+            def save_user():
+                msg = messagebox.showinfo(title="Estado do Cadastro", message="Parabens! Usuario cadastrado com sucesso.")
+                pass
+
+            save_button = ctk.CTkButton(master=rg_frame, text="Cadastrar", width=145, fg_color="green", hover_color="#2D9334", command=save_user)
+            save_button.place(x=180, y=310)
+           
+
+        cadastro_button = ctk.CTkButton(master=login_frame, text="Cadastrar usuario", width=150, fg_color="green", hover_color="#2D9334", command=tela_register)
         cadastro_button.place(x=175, y=330)
 
 
